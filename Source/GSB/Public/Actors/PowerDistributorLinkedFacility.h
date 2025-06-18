27c78e580a53590f89b58ba0d24a6941ded15c8a
hibiscus <yuy0815@icloud.com>
2025-06-18 20:55:41 +0900

    PowerDistributor

---
+++ Source/GSB/Public/Actors/PowerDistributorLinkedFacility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/ConstructibleFacility.h"
+#include "PowerDistributorLinkedFacility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GSB_API APowerDistributorLinkedFacility : public AConstructibleFacility
+{
+	GENERATED_BODY()
+	
+};
