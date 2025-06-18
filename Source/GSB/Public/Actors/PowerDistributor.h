05cea4fb0bfbff3a00d8fb8f3de27df064a36d33
hibiscus <yuy0815@icloud.com>
2025-06-18 14:27:00 +0900

    리팩토링

---
+++ Source/GSB/Public/Actors/PowerDistributor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/HubLinkedFacility.h"
+#include "PowerDistributor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GSB_API APowerDistributor : public AHubLinkedFacility
+{
+	GENERATED_BODY()
+	
+};
