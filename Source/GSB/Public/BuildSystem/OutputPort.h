645652f42ae160c9b95044f3dcd9c7b215c9c728
hibiscus <yuy0815@icloud.com>
2025-06-21 20:20:07 +0900

    리팩토링 및 버그 수정

---
+++ Source/GSB/Public/BuildSystem/OutputPort.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BuildSystem/ItemPort.h"
+#include "OutputPort.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GSB_API AOutputPort : public AItemPort
+{
+	GENERATED_BODY()
+	
+};
