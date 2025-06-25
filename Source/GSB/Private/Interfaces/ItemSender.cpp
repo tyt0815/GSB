92927ccd969253d382679d598d123363045ef774
hibiscus <yuy0815@icloud.com>
2025-06-26 00:29:22 +0900

    ItemReceiveComponent, ItemSendComponent, SplineTransportComponent

---
+++ Source/GSB/Private/Interfaces/ItemSender.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/ItemSender.h"
+
+// Add default functionality here for any IItemSender functions that are not pure virtual.
