637e43f911f81879d1592681ea83f332edde5456
hibiscus <yuy0815@icloud.com>
2025-06-29 15:54:00 +0900

    상호작용 UI 및 상호작용 로직

---
+++ Source/GSB/Private/Interfaces/InteractableActor.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/InteractableActor.h"
+
+// Add default functionality here for any IInteractableActor functions that are not pure virtual.
