9b22bd00b35b5675dda7c86e789e25c3007db03e
hibiscus <yuy0815@icloud.com>
2025-07-07 19:43:34 +0900

    Input 관련 PlayerController 클래스로 이관

---
+++ Source/GSB/Public/BuildSystem/GeneralFacilityBuilder.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BuildSystem/FacilityBuilder.h"
+#include "GeneralFacilityBuilder.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GSB_API AGeneralFacilityBuilder : public AFacilityBuilder
+{
+	GENERATED_BODY()
+	
+};
